a860a49fbab4956276a82bbd62607df779793a54
RFarkas <r4lv@peaxels.com>
2024-01-24 13:16:32 +0100

    prettify keymap

---
+++ config/keypos_qwertz.h
@@ -0,0 +1,70 @@
+/* 
+Beschreibt die Corne Tastatur mit den Tasten auf einer QWERTZ-Tastatur, auf der
+die Finger ungefähr liegen würden.
+
+  ╭─────────────────────────╮ ╭─────────────────────────╮
+  │ TAB  Q   W   E   R   T  │ │  Z   U   I   O   P   UE │
+  │ CPS  A   S   D   F   G  │ │  H   J   K   L   OE  AE │
+  │  LT  Y   X   C   V   B  │ │  N   M   ,  PNK MNS RSH │
+  ╰───────────╮ DL2 DL1 DL0 │ │ DR0 DR1 DR2 ╭───────────╯
+              ╰─────────────╯ ╰─────────────╯
+  ╭─────────────────────────╮ ╭────────────────────────╮
+  │  0   1   2   3   4   5  │ │  6   7   8   9  10  11 │
+  │ 12  13  14  15  16  17  │ │ 18  19  20  21  22  23 │
+  │ 24  25  26  27  28  29  │ │ 30  31  32  33  34  35 │
+  ╰───────────╮ 36  37  38  │ │ 39  40  41  ╭──────────╯
+              ╰─────────────╯ ╰─────────────╯            */
+
+#pragma once
+
+#define posTAB 0
+#define posQ 1
+#define posW 2
+#define posE 3
+#define posR 4
+#define posT 5
+#define posZ 6
+#define posU 7
+#define posI 8
+#define posO 9
+#define posP 10
+#define posUE 11
+
+#define posCAPS 12
+#define posCPS 12
+#define posA 13
+#define posS 14
+#define posD 15
+#define posF 16
+#define posG 17
+#define posH 18
+#define posJ 19
+#define posK 20
+#define posL 21
+#define posOE 22
+#define posAE 23
+
+#define posLT 24
+#define posY 25
+#define posX 26
+#define posC 27
+#define posV 28
+#define posB 29
+#define posN 30
+#define posM 31
+#define posKOMMA 32
+#define posKOM 32
+#define posPUNKT 33
+#define posPNK 33
+#define posMINUS 34
+#define posMNS 34
+#define posRSHIFT 35
+#define posRSH 35
+
+// Daumen
+#define posDL2 36
+#define posDL1 37
+#define posDL0 38
+#define posDR0 39
+#define posDR1 40
+#define posDR2 41
