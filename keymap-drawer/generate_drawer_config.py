fc2b38974ff8e4160e95f90254f569a618876bce
RFarkas <r4lv@peaxels.com>
2024-02-13 14:18:00 +0100

    keymap drawer: improve combo glyphs

--- keymap-drawer/generate_drawer_config.py
+++ keymap-drawer/generate_drawer_config.py
@@ -3,9 +3,12 @@
 Generates config.gen.yaml from config.template.yaml + style.gen.css + raw_binding_map.gen.yaml
 """
 from pathlib import Path
+from urllib.request import urlopen
+import re
+import io
+
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import LiteralScalarString as LS
-
 
 yaml = YAML()
 yaml.default_flow_style = False
@@ -20,10 +23,8 @@ cfg.yaml_set_start_comment(f"generated with {Path(__file__).name}")
 css = (HERE / "style.gen.css").read_text()
 cfg["draw_config"]["svg_style"] = LS(css + "\n" + cfg["draw_config"].get("svg_style", ""))
 
-for char in "abcdefghijklmnopqrstuvwxyz,.-":
-    glyph = cfg["draw_config"]["glyphs"]["r:hyper-TEMPLATE"].replace("(CHAR)", char.upper())
-    cfg["draw_config"]["glyphs"][f"r:hyper-{char}"] = glyph
-    cfg["parse_config"]["raw_binding_map"][f"&kp HYPER(DE_{char.upper()})"] = f"$$r:hyper-{char}$$"
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    cfg["parse_config"]["raw_binding_map"][f"&kp HYPER(DE_{char})"] = f"$$combo:atom/text:{char}$$"
 
 with open(HERE / "../de-mac/raw_binding_map.gen.yaml") as f:
     raw_binding_data = yaml.load(f)
@@ -32,6 +33,46 @@ with open(HERE / "../de-mac/raw_binding_map.gen.yaml") as f:
             print(f"Warning: key {k} already in template file")
         else:
             cfg["parse_config"]["raw_binding_map"][k] = v
+
+
+def get_glyph(key):
+    key = dict(alt="mdi:apple-keyboard-option").get(key, key)
+    if ":" not in key:
+        key = f"mdi:{key}"
+    group, key = key.split(":")
+    if group == "text":
+        return group, key, ""
+
+    url = cfg["draw_config"]["glyph_urls"][group].format(key)
+    try:
+        with urlopen(url) as f:
+            data = f.read().decode("utf-8")
+            data = re.sub(r"id=\".*?\"", "", data).replace("<svg", f"<svg id='rglyph:{key}'")
+            data = re.sub(r"viewBox=\"(.*?)\"", r"viewBox='\g<1>'", data, flags=re.IGNORECASE)
+    except Exception as e:
+        print(f"Error fetching {url}: {e}")
+        raise e
+    return group, key, data
+
+
+def yaml_dumps(data) -> str:
+    buf = io.StringIO()
+    yaml.dump(data, buf)
+    return buf.getvalue()
+
+
+for left, right in re.findall(r"\$\$combo:(.*?)/(.*?)\$\$", yaml_dumps(cfg)):
+    lgrp, lkey, ldef = get_glyph(left)
+    rgrp, rkey, rdef = get_glyph(right)
+    svg = f"""<svg viewBox="0 0 48 24"><defs>{ldef}{rdef}</defs>"""
+    svg += f"""<use href="#rglyph:{lkey}" x="-12" y="0" width="24" height="24"/>"""
+    if rgrp == "text":
+        svg += f"""<text x="24" y="14" style="font-size: 24px; text-anchor: start;">{rkey}</text>"""
+    else:
+        svg += f"""<use href="#rglyph:{rkey}" x="12" y="0" width="24" height="24" />"""
+    svg += """</svg>"""
+    cfg["draw_config"]["glyphs"][f"combo:{left}/{right}"] = LS(svg)
+
 
 with open(HERE / "config.gen.yaml", "w") as f:
     yaml.dump(cfg, f)
