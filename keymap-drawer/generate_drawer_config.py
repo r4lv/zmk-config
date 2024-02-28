aedd1772735d1a3a901d7b3d4ae8ac58aab95983
RFarkas <r4lv@peaxels.com>
2024-02-28 09:49:46 +0100

    draw: new ‚smallcombo‘ glyph helper
    the ‚text‘ glyphs now also return a ‚def‘ for <use>, instead of directly putting a <text> in place. This allows for better scaling using a (larger) viewbox for smallcombos.

--- keymap-drawer/generate_drawer_config.py
+++ keymap-drawer/generate_drawer_config.py
@@ -24,7 +24,9 @@ css = (HERE / "style.gen.css").read_text()
 cfg["draw_config"]["svg_style"] = LS(css + "\n" + cfg["draw_config"].get("svg_style", ""))
 
 for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-    cfg["parse_config"]["raw_binding_map"].setdefault(f"&kp HY(DE_{char})", f"$$combo:atom/text:{char}$$")
+    cfg["parse_config"]["raw_binding_map"].setdefault(
+        f"&kp HY(DE_{char})", f"$$combo:atom/text:{char}$$"
+    )
 
 with open(HERE / "../de-mac/raw_binding_map.gen.yaml") as f:
     raw_binding_data = yaml.load(f)
@@ -41,19 +43,26 @@ def get_glyph(key) -> tuple[str, str, str]:
         cmd="apple-keyboard-command",
         shift="apple-keyboard-shift",
         ctrl="apple-keyboard-control",
-    ).get(key, key)
+    ).get(key, key)  # short aliases for modifier keys
     if ":" not in key:
         key = f"mdi:{key}"
     group, key = key.split(":")
     if group == "text":
-        return group, key, ""
+        return (
+            group,
+            f"text-{key}",
+            f'<svg xmlns="http://www.w3.org/2000/svg" viewBox=\'0 0 24 24\' id="rglyph:text-{key}">'
+            + f'<text x="12" y="13" text-anchor="middle" style="font-size: 24px;">{key}</text>'
+            + '</svg>',
+        )
 
     url = cfg["draw_config"]["glyph_urls"][group].format(key)
     try:
         with urlopen(url) as f:
             # TODO: it would be nicer to re-use keymap-drawer's cache here.
             data = f.read().decode("utf-8")
-            data = re.sub(r"id=\".*?\"", "", data).replace("<svg", f"<svg id='rglyph:{key}'")
+            data = re.sub(r"id=\".*?\"", "", data)
+            data = data.replace("<svg", f"<svg id='rglyph:{key}'")
             data = re.sub(r"viewBox=\"(.*?)\"", r"viewBox='\g<1>'", data, flags=re.IGNORECASE)
             # this is a fix for nested SVGs - the regex in keymap-drawer always picks up the LAST
             # viewBox attribute with double quotes, so we use single quotes to hide the inner SVGs.
@@ -69,31 +78,29 @@ def yaml_dumps(data) -> str:
     return buf.getvalue()
 
 
-for left, middle, right in re.findall(r"\$\$combo:(.*?)/(.*?)/(.*?)\$\$", yaml_dumps(cfg)):
-    lgrp, lkey, ldef = get_glyph(left)
-    mgrp, mkey, mdef = get_glyph(middle)
-    rgrp, rkey, rdef = get_glyph(right)
-    svg = f"""<svg viewBox="0 0 72 24"><defs>{ldef}{mdef}{rdef}</defs>"""
-    svg += f"""<use href="#rglyph:{lkey}" x="-24" y="0" width="24" height="24"/>"""
-    svg += f"""<use href="#rglyph:{mkey}" x="0" y="0" width="24" height="24"/>"""
-    if rgrp == "text":
-        svg += f"""<text x="48" y="14" style="font-size: 24px; text-anchor: start;">{rkey}</text>"""
-    else:
-        svg += f"""<use href="#rglyph:{rkey}" x="24" y="0" width="24" height="24" />"""
-    svg += """</svg>"""
-    cfg["draw_config"]["glyphs"][f"combo:{left}/{middle}/{right}"] = LS(svg)
+for small, l, m, r in re.findall(r"\$\$(small)?combo:(.*?)/(.*?)/(.*?)\$\$", yaml_dumps(cfg)):
+    lgrp, lkey, ldef = get_glyph(l)
+    mgrp, mkey, mdef = get_glyph(m)
+    rgrp, rkey, rdef = get_glyph(r)
+    viewbox = "0 -6 72 36" if small else "0 0 72 24"
 
-for left, right in re.findall(r"\$\$combo:([^/]*?)/([^/]*?)\$\$", yaml_dumps(cfg)):
-    lgrp, lkey, ldef = get_glyph(left)
-    rgrp, rkey, rdef = get_glyph(right)
-    svg = f"""<svg viewBox="0 0 48 24"><defs>{ldef}{rdef}</defs>"""
-    svg += f"""<use href="#rglyph:{lkey}" x="-12" y="0" width="24" height="24"/>"""
-    if rgrp == "text":
-        svg += f"""<text x="24" y="14" style="font-size: 24px; text-anchor: start;">{rkey}</text>"""
-    else:
-        svg += f"""<use href="#rglyph:{rkey}" x="12" y="0" width="24" height="24" />"""
+    svg = f"""<svg viewBox="{viewbox}"><defs>{ldef}{mdef}{rdef}</defs>"""
+    # use single quotes to block _scrub_dims_re in keymap-drawer/glyph.py
+    svg += f"""<use href="#rglyph:{lkey}" x="0" y="0" width='24' height='24'/>"""
+    svg += f"""<use href="#rglyph:{mkey}" x="24" y="0" width='24' height='24'/>"""
+    svg += f"""<use href="#rglyph:{rkey}" x="48" y="0" width='24' height='24' />"""
     svg += """</svg>"""
-    cfg["draw_config"]["glyphs"][f"combo:{left}/{right}"] = LS(svg)
+    cfg["draw_config"]["glyphs"][f"{small}combo:{l}/{m}/{r}"] = LS(svg)
+
+for l, r in re.findall(r"\$\$combo:([^/]*?)/([^/]*?)\$\$", yaml_dumps(cfg)):
+    lgrp, lkey, ldef = get_glyph(l)
+    rgrp, rkey, rdef = get_glyph(r)
+    svg = f"""<svg viewBox="0 0 48 24"><defs>{ldef}{rdef}</defs>"""
+    # here, width and height are scrubbed by keymap-drawer
+    svg += f"""<use href="#rglyph:{lkey}" x="-12" y="0" width="24" height="24"/>"""
+    svg += f"""<use href="#rglyph:{rkey}" x="12" y="0" width="24" height="24" />"""
+    svg += """</svg>"""
+    cfg["draw_config"]["glyphs"][f"combo:{l}/{r}"] = LS(svg)
 
 
 with open(HERE / "config.gen.yaml", "w") as f:
