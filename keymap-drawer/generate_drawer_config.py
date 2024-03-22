a45bcb3b86059bcdbde9496a2f08258afd64f013
RFarkas <r4lv@peaxels.com>
2024-03-22 12:32:47 +0100

    rework combos

--- keymap-drawer/generate_drawer_config.py
+++ keymap-drawer/generate_drawer_config.py
@@ -42,14 +42,19 @@ with open(HERE / "../de-mac/raw_binding_map.gen.yaml") as f:
 
 
 def get_glyph(key) -> tuple[str, str, str]:
+    global cfg
     key = dict(
         alt="apple-keyboard-option",
         cmd="apple-keyboard-command",
         shift="apple-keyboard-shift",
         ctrl="apple-keyboard-control",
     ).get(key, key)  # short aliases for modifier keys
+
+    if key in cfg["draw_config"]["glyphs"]:
+        key = f"local:{key}"
     if ":" not in key:
         key = f"mdi:{key}"
+
     group, key = key.split(":")
     if group == "text":
         return (
@@ -59,20 +64,25 @@ def get_glyph(key) -> tuple[str, str, str]:
             + f'<text x="10" y="14" text-anchor="middle" style="font-size: 24px;">{key}</text>'
             + "</svg>",
         )
-
-    url = cfg["draw_config"]["glyph_urls"][group].format(key)
-    try:
-        with urlopen(url) as f:  # noqa: S310
-            # TODO: it would be nicer to re-use keymap-drawer's cache here.
-            data = f.read().decode("utf-8")
-            data = re.sub(r"id=\".*?\"", "", data)
-            data = data.replace("<svg", f"<svg id='rglyph:{key}'")
-            data = re.sub(r"viewBox=\"(.*?)\"", r"viewBox='\g<1>'", data, flags=re.IGNORECASE)
-            # this is a fix for nested SVGs - the regex in keymap-drawer always picks up the LAST
-            # viewBox attribute with double quotes, so we use single quotes to hide the inner SVGs.
-    except Exception as e:
-        print(f"Error fetching {url}: {e}")
-        raise e
+    elif group == "local":
+        data = cfg["draw_config"]["glyphs"][key]
+        data = re.sub(r"id=\".*?\"", "", data)
+        data = data.replace("<svg", f"<svg id='rglyph:{key}'")
+        data = re.sub(r"viewBox=\"(.*?)\"", r"viewBox='\g<1>'", data, flags=re.IGNORECASE)
+    else:
+        url = cfg["draw_config"]["glyph_urls"][group].format(key)
+        try:
+            with urlopen(url) as f:  # noqa: S310
+                # TODO: it would be nicer to re-use keymap-drawer's cache here.
+                data = f.read().decode("utf-8")
+                data = re.sub(r"id=\".*?\"", "", data)
+                data = data.replace("<svg", f"<svg id='rglyph:{key}'")
+                data = re.sub(r"viewBox=\"(.*?)\"", r"viewBox='\g<1>'", data, flags=re.IGNORECASE)
+                # this is a fix for nested SVGs - the regex in keymap-drawer always picks up the LAST
+                # viewBox attribute with double quotes, so we use single quotes to hide the inner SVGs.
+        except Exception as e:
+            print(f"Error fetching {url}: {e}")
+            raise e
     return group, key, data
 
 
